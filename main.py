3dd98c0c56718012b96c510e588dc73a0bb2c2f4
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2020-09-01 11:15:23 -0600

    Mas modelos
    Faltan shaders

--- main.py
+++ main.py
@@ -8,16 +8,24 @@ r.lookAt(V3(0, 0, 1), V3(0, 0, 0), V3(0, 1, 0))
 
 #baskett = Texture('./models/Basketball.bmp')
 #r.active_texture = baskett
-r.load('./models/sphere.obj', translate=(0, -0.55, 0), scale=(0.35, 0.35, 0.35), rotate=(0, 0, 0))
+r.load('./models/sphere.obj', translate=(0.08, -0.77, 0), scale=(0.27, 0.27, 0.27), rotate=(0, 0, 0))
 r.draw_arrays('FLAT')
 
 
 r.current_model = 'desk'
-r.load('./models/desk.obj', translate=(-0.6, -0.3, 0), scale=(0.0032, 0.0032, 0.0032), rotate=(0, 0, 0))
+r.load('./models/desk.obj', translate=(-0.65, -0.3, 0), scale=(0.003, 0.003, 0.003), rotate=(0, 0, 0))
 r.draw_arrays('FLAT')
 
+r.current_model = 'computer'
+r.load('./models/computer2.obj', translate=(-0.4, 0.19, 0), scale=(0.04, 0.04, 0.04), rotate=(0, 90, 0))
+r.draw_arrays('FLAT')
 
-r.load('./models/computer2.obj', translate=(-0.3, 0.3, 0), scale=(0.04, 0.045, 0.045), rotate=(0, 80, 0))
+'''r.current_model = 'computer'
+r.load('./models/Stand.obj', translate=(0.62, 0.62, 0), scale=(0.085, 0.085, 0.085), rotate=(0, 0, 0))
+r.draw_arrays('FLAT')'''
+
+r.current_model = 'computer'
+r.load('./models/shelf.obj', translate=(0.6, 0.5, 0), scale=(0.7, 0.7, 0.7), rotate=(0, 0, 0))
 r.draw_arrays('FLAT')
 
 r.display('out.bmp')
