08d78fa558e2f530db874e879af066d59c13c7c9
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2020-09-01 02:00:22 -0600

    Matrices

--- lib.py
+++ lib.py
@@ -1,39 +1,24 @@
 import struct
-import numpy
 from collections import namedtuple
 
-# ===============================================================
-# Math
-# ===============================================================
+#Extraido de https://stackoverflow.com/questions/28253102/python-3-multiply-a-vector-by-a-matrix-without-numpy
+def matrixMul(a, b):
+    c = []
+    for i in range(0,len(a)):
+        temp=[]
+        for j in range(0,len(b[0])):
+            s = 0
+            for k in range(0,len(a[0])):
+                s += a[i][k]*b[k][j]
+            temp.append(s)
+        c.append(temp)
 
-# Vertex3Type = numpy.dtype([('x', 'f4'), ('y', 'f4'), ('z', 'f4')])
-# Vertex2Type = numpy.dtype([('x', 'f4'), ('y', 'f4')])
+    return c
 
-class V3(object):
-  def __init__(self, x, y = None, z = None):
-    if (type(x) == numpy.matrix):
-      self.x, self.y, self.z = x.tolist()[0]
-    else:
-      self.x = x
-      self.y = y
-      self.z = z
+#Funciones dadas por dennis
 
-  def __repr__(self):
-    return "V3(%s, %s, %s)" % (self.x, self.y, self.z)
-
-class V2(object):
-  def __init__(self, x, y = None):
-    if (type(x) == numpy.matrix):
-      self.x, self.y = x.tolist()[0]
-    else:
-      self.x = x
-      self.y = y
-
-  def __repr__(self):
-    return "V2(%s, %s)" % (self.x, self.y)
-
-# V2 = namedtuple('Point2', ['x', 'y'])
-# V3 = namedtuple('Point3', ['x', 'y', 'z'])
+V2 = namedtuple('Vertex2', ['x', 'y'])
+V3 = namedtuple('Vertex3', ['x', 'y', 'z'])
 
 def sum(v0, v1):
   """
@@ -42,13 +27,6 @@ def sum(v0, v1):
   """
   return V3(v0.x + v1.x, v0.y + v1.y, v0.z + v1.z)
 
-def sub(v0, v1):
-  """
-    Input: 2 size 3 vectors
-    Output: Size 3 vector with the per element substraction
-  """
-  return V3(v0.x - v1.x, v0.y - v1.y, v0.z - v1.z)
-
 def mul(v0, k):
   """
     Input: 2 size 3 vectors
@@ -56,36 +34,58 @@ def mul(v0, k):
   """
   return V3(v0.x * k, v0.y * k, v0.z *k)
 
-def dot(v0, v1):
-  """
-    Input: 2 size 3 vectors
-    Output: Scalar with the dot product
-  """
-  return v0.x * v1.x + v0.y * v1.y + v0.z * v1.z
+def char(c):
+    return struct.pack('=c', c.encode('ascii'))
+
+def word(c):
+    return struct.pack('=h', c)
+
+def dword(c):
+    return struct.pack('=l', c)
+
+def glCreateWindow(width, height):
+        win = Render(width, height)
+        return win
 
 def cross(v0, v1):
-  """
-    Input: 2 size 3 vectors
-    Output: Size 3 vector with the cross product
-  """
+  #Producto cruz de 2 vectores
   return V3(
     v0.y * v1.z - v0.z * v1.y,
     v0.z * v1.x - v0.x * v1.z,
     v0.x * v1.y - v0.y * v1.x,
   )
 
-def length(v0):
-  """
-    Input: 1 size 3 vector
-    Output: Scalar with the length of the vector
-  """
-  return (v0.x**2 + v0.y**2 + v0.z**2)**0.5
+def color(r, g, b):
+    return(bytes([b, g, r]))
+
+def bbox(*vertices):
+  #Bounding box desde 2 vectores
+  xs = [ vertex.x for vertex in vertices ]
+  ys = [ vertex.y for vertex in vertices ]
+  xs.sort()
+  ys.sort()
+
+  return V2(xs[0], ys[0]), V2(xs[-1], ys[-1])
+
+def barycentric(A, B, C, P):
+  #Conseguir coordenadas baricentricas desde los 3 vectores con producto cruz 
+  cx, cy, cz = cross(
+    V3(B.x - A.x, C.x - A.x, A.x - P.x), 
+    V3(B.y - A.y, C.y - A.y, A.y - P.y)
+  )
+
+  if abs(cz) < 1:
+      #es triangulo degenerado (regresar lo que sea)
+    return -1, -1, -1
+
+  u = cx/cz
+  v = cy/cz
+  w = 1 - (u + v)
+
+  return w, v, u
 
 def norm(v0):
-  """
-    Input: 1 size 3 vector
-    Output: Size 3 vector with the normal of the vector
-  """
+  #Normal del vector
   v0length = length(v0)
 
   if not v0length:
@@ -93,39 +93,16 @@ def norm(v0):
 
   return V3(v0.x/v0length, v0.y/v0length, v0.z/v0length)
 
-def bbox(*vertices):
-  """
-    Input: n size 2 vectors
-    Output: 2 size 2 vectors defining the smallest bounding rectangle possible
-  """
-  xs = [ vertex.x for vertex in vertices ]
-  ys = [ vertex.y for vertex in vertices ]
-  xs.sort()
-  ys.sort()
+def dot(v0, v1):
+  #Producto punto
+  return v0.x * v1.x + v0.y * v1.y + v0.z * v1.z
 
-  return V2(int(xs[0]), int(ys[0])), V2(int(xs[-1]), int(ys[-1]))
+def sub(v0, v1):
+    #Resta de vectores
+  return V3(v0.x - v1.x, v0.y - v1.y, v0.z - v1.z)
 
-
-def barycentric(A, B, C, P):
-  """
-    Input: 3 size 2 vectors and a point
-    Output: 3 barycentric coordinates of the point in relation to the triangle formed
-            * returns -1, -1, -1 for degenerate triangles
-  """
-  bary = cross(
-    V3(C.x - A.x, B.x - A.x, A.x - P.x),
-    V3(C.y - A.y, B.y - A.y, A.y - P.y)
-  )
-
-  if abs(bary.z) < 1:
-    return -1, -1, -1   # this triangle is degenerate, return anything outside
-
-  return (
-    1 - (bary.x + bary.y) / bary.z,
-    bary.y / bary.z,
-    bary.x / bary.z
-  )
-
+def length(v0):
+  return (v0.x**2 + v0.y**2 + v0.z**2)**0.5
 
 def allbarycentric(A, B, C, bbox_min, bbox_max):
   barytransform = numpy.linalg.inv([[A.x, B.x, C.x], [A.y,B.y,C.y], [1, 1, 1]])
@@ -135,56 +112,6 @@ def allbarycentric(A, B, C, bbox_min, bbox_max):
   # barycoords = barycoords[:,numpy.all(barycoords>=0, axis=0)]
   barycoords = numpy.transpose(barycoords)
   return barycoords
-
-
-# ===============================================================
-# Utils
-# ===============================================================
-
-
-def char(c):
-  """
-  Input: requires a size 1 string
-  Output: 1 byte of the ascii encoded char
-  """
-  return struct.pack('=c', c.encode('ascii'))
-
-def word(w):
-  """
-  Input: requires a number such that (-0x7fff - 1) <= number <= 0x7fff
-         ie. (-32768, 32767)
-  Output: 2 bytes
-  Example:
-  >>> struct.pack('=h', 1)
-  b'\x01\x00'
-  """
-  return struct.pack('=h', w)
-
-def dword(d):
-  """
-  Input: requires a number such that -2147483648 <= number <= 2147483647
-  Output: 4 bytes
-  Example:
-  >>> struct.pack('=l', 1)
-  b'\x01\x00\x00\x00'
-  """
-  return struct.pack('=l', d)
-
-def color(r, g, b):
-  """
-  Input: each parameter must be a number such that 0 <= number <= 255
-         each number represents a color in rgb
-  Output: 3 bytes
-  Example:
-  >>> bytes([0, 0, 255])
-  b'\x00\x00\xff'
-  """
-  return bytes([b, g, r])
-
-
-# ===============================================================
-# BMP
-# ===============================================================
 
 def writebmp(filename, width, height, pixels):
   f = open(filename, 'bw')
